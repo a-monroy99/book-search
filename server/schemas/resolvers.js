db2b1e1182efb72ad35b8d34eccfb180ae7af1ef
a.liz <amanda.monroy4@gmail.com>
2023-08-14 23:20:36 -0700

    modifying resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,7 @@
 const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
@@ -13,5 +14,30 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user  = await User.create({ username, email, password });
+      const token = signToken(user)
+
+      return { token, user }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user with this email found');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Your password is incorrect')
+      }
+    
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: 
   }
 }
